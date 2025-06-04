6a9afaf11141ce3c2bca452d993d9b96b5de06f3
edrav <eden.ravenwood@gmail.com>
2025-06-05 00:22:54 +1000

    Aggregation of training data into site, date, min humidity, max humidity, min temperature, max temperature

--- model-trainer.php
+++ model-trainer.php
@@ -13,21 +13,23 @@ $headers = array_map('trim', array_shift($data));           # remove header row 
 $records = [];
 foreach ($data as $row) {
     # extract values from the row
-    $site = (int)($row[0]);                           #
+    $site = (int)($row[0]);
     $date = $row[1];
-    $humidity = (float)$row[2];
-    $temperature = ($row)[3];
+    $humidity = (int)$row[2];
+    $temperature = (float)$row)[3];
+    [$year, $month, $day] = explode('-', $date);
 
-    if (!isset($records[$site][$date])) {
-        $records[$site][$date] = [
+    if (!isset($records[$site][$date])) {               # check for existing record for the current site/date
+        $records[$site][$date] = [                      # create a new record if none exists
             'site' => $site,
-            'date' => $date,
+            'month' => (int)$month,
+            'day' => (int)$day,
             'min_humidity' => $humidity,
             'max_humidity' => $humidity,
             'min_temperature' => $temperature,
             'max_temperature' => $temperature,
         ];
-    } else {
+    } else {                                            # if a record already exists compare it to the current row temp/humidity and keep the current row value if it is a new min/max
         $records[$site][$date]['min_humidity'] = min($records[$site][$date]['min_humidity'], $humidity);
         $records[$site][$date]['max_humidity'] = max($records[$site][$date]['max_humidity'], $humidity);
         $records[$site][$date]['min_temperature'] = min($records[$site][$date]['min_temperature'], $temperature);
@@ -36,6 +38,12 @@ foreach ($data as $row) {
     }
 }
 
-
+$samples = [];
+$labels = [
+    'min_humidity' => [],
+    'max_humidity' => [],
+    'min_temperature' => [],
+    'max_temperature' => []
+];
 
 ?>
