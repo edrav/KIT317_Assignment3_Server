7f708de6f021b5e8816a4095d778855aabf90574
edrav <eden.ravenwood@gmail.com>
2025-06-06 23:19:00 +1000

    Implemented code to train SVR models for each predicted variable

--- model-trainer.php
+++ model-trainer.php
@@ -3,6 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Phpml\Regression\SVR;
 use Phpml\SupportVectorMachine\Kernel;
+use Phpml\ModelManager;
 
 $input_csv = 'training_data.csv';
 $output_json = 'training_data.json';
@@ -60,4 +61,44 @@ foreach ($records as $site => $siteData) {
         $labels['max_temperature'][] = (float)$values['max_temperature'];
     }
 }
+
+
+
+$svr_minHumidity = new SVR(Kernel::RBF);
+$svr_maxHumidity = new SVR(Kernel::RBF);
+$svr_minTemperature = new SVR(Kernel::RBF);
+$svr_maxTemperature = new SVR(Kernel::RBF);
+
+$modelManager = new ModelManager();
+
+
+echo "Training minimum humidity started...\n";
+$start_minHumidity = microtime(true);
+$svr_minHumidity->train($samples, $labels['min_humidity']);
+$modelManager->saveToFile($svr_minHumidity, 'minHumidity.svr');
+$end_minHumidity = microtime(true);
+echo "Training minimum humidity completed in " . round($end_minHumidity - $start_minHumidity, 2) . " seconds.\n";
+
+echo "Training maximum humidity started...\n";
+$start_maxHumidity = microtime(true);
+$svr_maxHumidity->train($samples, $labels['max_humidity']);
+$modelManager->saveToFile($svr_maxHumidity, 'maxHumidity.svr');
+$end_maxHumidity = microtime(true);
+echo "Training maximum humidity completed in " . round($end_maxHumidity - $start_maxHumidity, 2) . " seconds.\n";
+
+echo "Training minimum temperature started...\n";
+$start_minTemperature = microtime(true);
+$svr_minTemperature->train($samples, $labels['min_temperature']);
+$modelManager->saveToFile($svr_minTemperature, 'minTemperature.svr');
+$end_minTemperature = microtime(true);
+echo "Training minimum temperature completed in " . round($end_minTemperature - $start_minTemperature, 2) . " seconds.\n";
+
+echo "Training maximum temperature started...\n";
+$start_maxTemperature = microtime(true);
+$svr_maxTemperature->train($samples, $labels['max_temperature']);
+$modelManager->saveToFile($svr_maxTemperature, 'maxTemperature.svr');
+$end_maxTemperature = microtime(true);
+echo "Training maximum temperature completed in " . round($end_maxTemperature - $start_maxTemperature, 2) . " seconds.\n";
+
+
 ?>
