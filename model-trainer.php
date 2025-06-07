047db84cf05daca9052ff256602ad14a9759d126
edrav <eden.ravenwood@gmail.com>
2025-06-07 19:47:56 +1000

    created prediction.php and moved shared functions to functions.php.

--- model-trainer.php
+++ model-trainer.php
@@ -7,35 +7,9 @@ use Phpml\Regression\SVR;
 use Phpml\SupportVectorMachine\Kernel;
 use Phpml\ModelManager;
 
+include 'functions.php';
+
 $input_csv = 'training_data.csv';
-
-/*
-convertToDayOfYear()
-turns month and day to just day of the year e.g. March 3rd becomes 31+28+3=62
-prevents inference of relationship between the same day of each month, e.g the 10th of march and the 10th of
-september have no specific relationship
-*/
-Function convertToDayOfYear($m, $d) {  # turns month and day to just day of the year e.g. March 3rd becomes 31+28+3=62
-    $totalDays = 0;
-    static $daysInMonth = [ 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, ];
-    for ($i=0; $i < $m; $i++) {
-        $totalDays = $totalDays + $daysInMonth[$i];
-    }
-
-    $totalDays = $totalDays + $d;
-
-    return $totalDays;
-}
-
-/*
-matchSiteID()
-Used to produce $sample training dataset with no numeric relationship between sites.  e.g. Site ID's are arbitrary and
-There is nothing to be inferred from the fact that site '2' is in between sites '1' and '3' or that it is half of site '4'
- */
-Function matchSiteID($staticSite, $variableSite) {
-    return $staticSite === $variableSite ? 1 : 0;
-}
-
 
 // load data
 echo "Loading $input_csv\n";
@@ -47,10 +21,7 @@ $headers = array_map('trim', array_shift($data));       # remove header row and 
 $end_dataLoad = microtime(true);
 echo "Loading $input_csv completed in " . round($end_dataLoad - $start_dataLoad, 2) . " seconds.\n";
 
-
-
 // process data
-
 echo "Processing $input_csv\n";
 $start_dataProcessing = microtime(true);
 
---
+++ prediction.php
@@ -0,0 +1,109 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Phpml\Regression\SVR;
+use Phpml\SupportVectorMachine\Kernel;
+use Phpml\ModelManager;
+
+include 'functions.php';
+
+$modelManager = new ModelManager();
+$svr_minHumidity = new SVR(Kernel::RBF);
+$svr_maxHumidity = new SVR(Kernel::RBF);
+$svr_minTemperature = new SVR(Kernel::RBF);
+$svr_maxTemperature = new SVR(Kernel::RBF);
+$modelMissing = False;
+
+
+$minHumidity_file = 'minHumidity.svc';
+if (file_exists($minHumidity_file)) {
+    $svc_minHumidity = $modelManager->restoreFromFile($minHumidity_file);
+} else {
+    $modelMissing = True;
+}
+
+$maxHumidity_file = 'maxHumidity.svc';
+if (file_exists($maxHumidity_file)) {
+    $svc_maxHumidity = $modelManager->restoreFromFile($maxHumidity_file);
+} else {
+    $modelMissing = True;
+}
+
+$minTemperature_file = 'minTemperature.svc';
+if (file_exists($minTemperature_file)) {
+    $svc_minTemperature = $modelManager->restoreFromFile($minTemperature_file);
+} else {
+    $modelMissing = True;
+}
+
+$maxTemperature_file = 'maxTemperature.svc';
+if (file_exists($maxTemperature_file)) {
+    $svc_maxTemperature = $modelManager->restoreFromFile($maxTemperature_file);
+} else {
+    $modelMissing = True;
+}
+
+if ($modelMissing) {
+    http_response_code(503);
+    exit('One or more SVR files do not exist.');
+}
+
+$month = $_GET['month'] ?? null;
+$dayOfMonth = $_GET['day'] ?? null;
+$site = isset($_GET['site']) ? (int)$_GET['site'] : null;
+
+if (!$month || !$dayOfMonth || !$site) {
+    http_response_code(400);
+    exit('One or more parameters missing from URI.');
+}
+
+switch ($site) {
+    case 1:
+        $siteName = 'Wynard';
+        break;
+    case 2:
+        $siteName = 'Launceston';
+        break;
+    case 3:
+        $siteName = 'Smithton';
+        break;
+    case 4:
+        $siteName = 'Hobart';
+        break;
+    case 5:
+        $siteName = 'Campania';
+        break;
+    default:
+        http_response_code(400);
+        exit('Site not found.');
+}
+
+$dayOfYear = convertToDayOfYear($month, $dayOfMonth);
+$sample = [
+    $dayOfYear,
+    (int)matchSiteID(1, $site),
+    (int)matchSiteID(2, $site),
+    (int)matchSiteID(3, $site),
+    (int)matchSiteID(4, $site),
+    (int)matchSiteID(5, $site)
+];
+
+$pred_minHumidity = $svr_minHumidity->predict($sample);
+$pred_maxHumidity = $svr_maxHumidity->predict($sample);
+$pred_minTemperature = $svr_minTemperature->predict($sample);
+$pred_maxTemperature = $svr_maxTemperature->predict($sample);
+
+$prediction = [
+    'minTemp' => $pred_minTemperature,
+    'maxTemp' => $pred_maxTemperature,
+    'minHumi' => $pred_minHumidity,
+    'maxHumi' => $pred_maxHumidity,
+    'locName' => $siteName
+];
+
+$response = json_encode((object)$prediction);
+http_response_code(200);
+header('Content-type: application/json');
+echo $response;
+
+?>
