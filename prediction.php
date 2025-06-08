97e2bc430582b9e9e78b4098c6b5f87baab3fcbe
edrav <eden.ravenwood@gmail.com>
2025-06-08 12:33:04 +1000

    removed forced temp code,   added BASE_PATH

--- prediction.php
+++ prediction.php
@@ -17,28 +17,28 @@ $svr_maxTemperature = new SVR(Kernel::RBF);
 $modelMissing = False;
 
 
-$minHumidity_file = BASE_PATH . 'minHumidity.svr';
+$minHumidity_file = BASE_PATH . '/minHumidity.svr';
 if (file_exists($minHumidity_file)) {
     $svc_minHumidity = $modelManager->restoreFromFile($minHumidity_file);
 } else {
     $modelMissing = True;
 }
 
-$maxHumidity_file = BASE_PATH . 'maxHumidity.svr';
+$maxHumidity_file = BASE_PATH . '/maxHumidity.svr';
 if (file_exists($maxHumidity_file)) {
     $svc_maxHumidity = $modelManager->restoreFromFile($maxHumidity_file);
 } else {
     $modelMissing = True;
 }
 
-$minTemperature_file = BASE_PATH . 'minTemperature.svr';
+$minTemperature_file = BASE_PATH . '/minTemperature.svr';
 if (file_exists($minTemperature_file)) {
     $svc_minTemperature = $modelManager->restoreFromFile($minTemperature_file);
 } else {
     $modelMissing = True;
 }
 
-$maxTemperature_file = BASE_PATH . 'maxTemperature.svr';
+$maxTemperature_file = BASE_PATH . '/maxTemperature.svr';
 if (file_exists($maxTemperature_file)) {
     $svc_maxTemperature = $modelManager->restoreFromFile($maxTemperature_file);
 } else {
