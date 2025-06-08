fb18cfa70d1fefacb2b36b776680c5f645abed0f
edrav <eden.ravenwood@gmail.com>
2025-06-08 17:02:29 +1000

    fixed some typos

--- graph.php
+++ graph.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 $historicalDataFileName = 'training_data.csv';
 $predictionDataFileName = 'graph_input.json';
 
